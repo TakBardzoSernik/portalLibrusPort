8b7c215074a44912835f1c1b1419ce1b44dc04e4
dxmdeveloper <74650257+dxmdeveloper@users.noreply.github.com>
2020-11-18 20:44:26 +0100

    Update Oceny.java
    * Selektory CSS final na początku klasy
    * Oceny z drugiego semestru.

--- src/com/company/category/Oceny.java
+++ src/com/company/category/Oceny.java
@@ -19,6 +19,8 @@ public class Oceny {
     final int iTerm2        = 4;
     final int iEndTerm2     = 6;
     final int iFinal        = 7;
+    final String rowsCssQuery = ">tr:not([name])";
+    final String cellsCssQuery = ">td:not([class=\"center micro screen-only\"])";
 
     List<Ocena> ocenyS1 = new ArrayList<>();
     List<Ocena> ocenyS2 = new ArrayList<>();
@@ -41,14 +43,14 @@ public class Oceny {
     public void collectData(Document htmlDoc){
 
         Element table =  htmlDoc.select("tbody").get(iTbody);
-        Elements rows = table.select(">tr:not([name])");
+        Elements rows = table.select(rowsCssQuery);
         przedmioty = new Przedmiot[rows.size()];
         int i = 0;
         System.out.println("Liczba przedmiotów: "+rows.size());
 
         for (Element row : rows)  {
             przedmioty[i] = new Przedmiot();
-            Elements cells = row.select(">td:not([class])");
+            Elements cells = row.select(cellsCssQuery);
 
             //Nazwa przedmiotu
             przedmioty[i].name = cells.get(iSubjectName).text();
@@ -57,17 +59,17 @@ public class Oceny {
             ocenySemestru(ocenyS1, cells, iTerm1, i);
 
             //Ocena Semestralna 1
-            if(cells.size() == 3) break;
+            if(cells.size() < 6) break;
             ocenaIndywidualna(semestralna1, cells, iEndTerm1, i);
 
             //Oceny Semestr 2
-            //ocenySemestru(ocenyS2, cells, iTerm2, i);
+            ocenySemestru(ocenyS2, cells, iTerm2, i);
 
             //Ocena Semestralna 2
-            //ocenaIndywidualna(semestralna2, cells, iEndTerm2, i);
+            ocenaIndywidualna(semestralna2, cells, iEndTerm2, i);
 
             //Ocena Końcowa
-            //ocenaIndywidualna(koncowa, cells, iFinal, i);
+            ocenaIndywidualna(koncowa, cells, iFinal, i);
             i++;
         };
 
